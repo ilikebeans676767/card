4e63dc76e25c221b3e145fcca9885f5ee79278c2
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-06-22 13:02:36 +0700

    better cloud save check 4

--- js/popups/save.js
+++ js/popups/save.js
@@ -129,9 +129,12 @@ popups.save = {
                 saveGame = () => { return false; }
                 callPopup("prompt", i18n.busy_import(), i18n.busy_desc(), {});
                 localStorage.setItem(SAVE_KEY, LZString.compress(JSON.stringify(importData)));
-                if (cloud.save) saveToCloud(0, () => {
-                    window.location.reload();
-                }, true);
+                if (cloud.save) {
+                    game = importData;
+                    saveToCloud(0, () => {
+                        window.location.reload();
+                    }, true);
+                }
                 else window.location.reload();
             }
         });
